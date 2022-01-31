84276ee614d5cc278da5303348b0d096ce0460fa
Prashant Kumar Upadhyay <78552170+2507Prashantkumarupadhyay@users.noreply.github.com>
2022-01-31 15:43:46 +0530

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -4,3 +4,16 @@ yy=int(input("Enter the year : "))
 mm=int(input("Enter the Month : "))
 
 print(calendar.month(yy,mm))
+
+# Output:
+"""
+Enter the year : 2022
+Enter the Month : 2
+   February 2022
+Mo Tu We Th Fr Sa Su
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28
+"""

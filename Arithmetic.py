8b292791d4e4dc23bdc2e519dd93fdf592353969
Prashant Kumar Upadhyay <78552170+2507Prashantkumarupadhyay@users.noreply.github.com>
2022-06-18 08:26:52 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,18 @@
+
+# Python Operators: 
+"""
+1.Python Operators in general are used to perform operations on values and variables. 
+2.These are standard symbols used for the purpose of logical and arithmetic operations. 
+3.In this article, we will look into different types of Python operators. 
+"""
+
+
+# Arithmetic Operator:
+'''Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, and division.'''
+x=34
+y=90
+print(x+y) #Addition
+print(x-y) #Substraction
+print(x*y) #Multiplication 
+print(x/y) #division(float)
+print(x//y) #division
